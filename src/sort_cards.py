cebce43ac0f8fdebb84cb0b6e1d59cbf844dbe34
Nathan Moore <ncmoore1986@gmail.com>
2017-12-15 13:54:44 -0800

    hopefully this solution is acceptable

--- src/sort_cards.py
+++ src/sort_cards.py
@@ -1,2 +1,22 @@
 def sort_cards(cards):
-    return sorted(cards, key="A23456789TJQK".index)
+    output = []
+    for card in cards:
+        if card.isdigit():
+            output.append(card)
+    output = sorted(output)
+    for card in cards:
+        if card == 'A':
+            output.insert(0, 'A')
+    for card in cards:
+        if card == 'T':
+            output.append('T')
+    for card in cards:
+        if card == 'J':
+            output.append('J')
+    for card in cards:
+        if card == 'Q':
+            output.append('Q')
+    for card in cards:
+        if card == 'K':
+            output.append('K')
+    return output
